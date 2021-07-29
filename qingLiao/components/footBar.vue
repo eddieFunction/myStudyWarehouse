da267824465b06a1336dac6b44756bc451db736d
Eddie Chen <57659325+eddieFunction@users.noreply.github.com>
2021-07-29 14:42:44 +0800

    Add files via upload

---
+++ qingLiao/components/footBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <van-tabbar v-model="active">
+          <van-tabbar-item replace to='/discover' icon='chat-o'>发现</van-tabbar-item>
+          <van-tabbar-item replace to='/friends' icon='friends-o'>朋友</van-tabbar-item>
+          <van-tabbar-item replace to='/profile' icon='user-o'>我的</van-tabbar-item>
+      </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            active: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
