d872e53abb68d48375d7404eef4f0e9d249ad5ad
Eddie Chen <57659325+eddieFunction@users.noreply.github.com>
2021-07-29 14:34:41 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vant from 'vant'
+import { Lazyload } from 'vant';
+import 'vant/lib/index.css';
+
+
+createApp(App).use(store).use(router).use(vant).use(Lazyload).mount('#app')
